2f148d3911ca047fc824813fa1e24dd5bdfc9784
Convolutio <79534557+Convolutio@users.noreply.github.com>
2026-01-12 17:42:09 +0100

    feat: output classes distribution analysis

---
+++ src/data_info.py
@@ -0,0 +1,121 @@
+"""Open the dataloaders as set in the hydra config and log some data analyses."""
+
+from typing import Any, Dict, List, Tuple, cast
+
+import hydra
+import rootutils
+import torch
+from lightning import LightningDataModule
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig
+
+rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+# ------------------------------------------------------------------------------------ #
+# the setup_root above is equivalent to:
+# - adding project root dir to PYTHONPATH
+#       (so you don't need to force user to install project as a package)
+#       (necessary before importing any local modules e.g. `from src import utils`)
+# - setting up PROJECT_ROOT environment variable
+#       (which is used as a base for paths in "configs/paths/default.yaml")
+#       (this way all filepaths are the same no matter where you run the code)
+# - loading environment variables from ".env" in root dir
+#
+# you can remove it if you:
+# 1. either install project as a package or move entry files to project root dir
+# 2. set `root_dir` to "." in "configs/paths/default.yaml"
+#
+# more info: https://github.com/ashleve/rootutils
+# ------------------------------------------------------------------------------------ #
+
+from src.utils import (
+    RankedLogger,
+    extras,
+    instantiate_loggers,
+    log_hyperparameters,
+    task_wrapper,
+)
+
+log = RankedLogger(__name__, rank_zero_only=True)
+
+
+def get_frequencies(targets: torch.Tensor):
+    """Return the unique target class and their occurrences."""
+    targets = cast(torch.Tensor, targets)
+    encoding = (
+        targets
+        * (2 ** torch.arange(3, dtype=torch.int32, device=targets.device))[torch.newaxis, :]
+    ).sum(
+        dim=1
+    )  # shape (B,)
+    return torch.unique(encoding, return_counts=True)
+
+
+@task_wrapper
+def data_analysis(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+    """Evaluates given checkpoint on a datamodule testset.
+
+    This method is wrapped in optional @task_wrapper decorator, that controls the behavior during
+    failure. Useful for multiruns, saving info about the crash, etc.
+
+    :param cfg: DictConfig configuration composed by Hydra.
+    :return: Tuple[dict, dict] with metrics and dict with all instantiated objects.
+    """
+
+    log.info(f"Instantiating datamodule <{cfg.data._target_}>")
+    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
+
+    log.info("Instantiating loggers...")
+    logger: List[Logger] = instantiate_loggers(cfg.get("logger"))
+
+    object_dict = {
+        "cfg": cfg,
+        "datamodule": datamodule,
+        "logger": logger,
+    }
+
+    log.info("Starting analysis!")
+    datamodule.prepare_data()
+    datamodule.setup("fit")
+
+    NUM_STEM_CLASSES = 8
+    train_val_class_frequencies = torch.zeros((NUM_STEM_CLASSES,), dtype=torch.int32)
+    test_class_freqencies = torch.zeros((NUM_STEM_CLASSES,), dtype=torch.int32)
+
+    for _, targets in iter(datamodule.train_dataloader()):
+        classes, counts = get_frequencies(targets)
+        train_val_class_frequencies[classes] += counts
+
+    for _, targets in iter(datamodule.val_dataloader()):
+        classes, counts = get_frequencies(targets)
+        train_val_class_frequencies[classes] += counts
+
+    log.info(
+        f"Got train/valid class distribution:\n{train_val_class_frequencies / train_val_class_frequencies.sum()}"
+    )
+
+    for _, targets in iter(datamodule.test_dataloader()):
+        classes, counts = get_frequencies(targets)
+        test_class_freqencies[classes] += counts
+
+    log.info(
+        f"Got test class distribution:\n{test_class_freqencies / test_class_freqencies.sum()}"
+    )
+
+    return {}, object_dict
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="data_analysis.yaml")
+def main(cfg: DictConfig) -> None:
+    """Main entry point for the datamodule's analysis.
+
+    :param cfg: DictConfig configuration composed by Hydra.
+    """
+    # apply extra utilities
+    # (e.g. ask for tags if none are provided in cfg, print cfg tree, etc.)
+    extras(cfg)
+
+    data_analysis(cfg)
+
+
+if __name__ == "__main__":
+    main()
